c084ca00212e94d85efeb46136446f1925b822ad
K.Stepanov <unholy.vostok@gmail.com>
2016-04-13 20:16:52 +0300

    Add respond json format for soups

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,11 +1,37 @@
 class SoupsController < ApplicationController
   def show
     @soup = Soup.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json{ render json: @soup }
+    end
   end
 
-  respond_to do |format|
-    format.html
-    format.json
+  def index
+    @soups = Soup.all
+
+    respond_to do |format|
+      format.json { render json: @soups }
+    end
+  end
+
+    def new
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
   end
 
 end
